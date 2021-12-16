02cfbaa999587de67eb133e4b7c7fe683e4dc3c8
Wellington Cardoso <wellington.acardoso18@gmail.com>
2021-12-16 17:56:23 -0300

    Initial commit

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,10 @@
+import {IsString} from 'class-validator';
+import { User } from "../entities/user.entity";
+
+export class CreateUserDto extends User {
+    @IsString()
+    name: string;
+
+    @IsString()
+    email: string;    
+}
